1da009c8e6f429c3cfa030679217e9a94add7ecd
dinhthihanh-dth <dinhthihanh138@gmail.com>
2025-08-30 23:07:32 +0700

    add Project 3

--- src/Components/MyWork/MyWork.jsx
+++ src/Components/MyWork/MyWork.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './MyWork.css';
-import { Element } from "react-scroll"; // thêm Element để cuộn đến
+import { Element } from "react-scroll"; 
 
 
 const MyWork = () => {
@@ -12,7 +12,7 @@ const MyWork = () => {
                     <h1>Projects</h1>
                 </div>
                 <div className="mywork-container">
-                    {/* Each image is wrapped in an anchor tag to link to the project */}
+                  
                     <div className="mywork-item">
                         <a href="https://thoi-trang-bigsize.netlify.app/" target="_blank" rel="noopener noreferrer">
                         <img src="/image_1.jpg" alt="Project 1" />
@@ -24,8 +24,8 @@ const MyWork = () => {
                         </a>
                     </div>
                     <div className="mywork-item">
-                        <a href="https://link-to-your-project-3.com" target="_blank" rel="noopener noreferrer">
-                            <img src="path-to-your-image-3.jpg" alt="Project 3" />
+                        <a href="https://food-onl-frontend.onrender.com" target="_blank" rel="noopener noreferrer">
+                            <img src="header_img.png" alt="Project 3" />
                         </a>
                     </div>
                     <div className="mywork-item">
